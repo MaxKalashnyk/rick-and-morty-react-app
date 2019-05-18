74e101c351f680c29105ffde431fbf5079cf83e3
kalash14 <maxim.kalashnyk@gmail.com>
2019-05-18 12:43:27 +0300

    Updates

---
+++ precache-manifest.3a4db98d4db184b0201233e87aec95be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9961ab70e6ebb214123",
+    "url": "/rick-and-morty-react-app/static/js/runtime~main.433d53d3.js"
+  },
+  {
+    "revision": "626f47f4063f80c7ebc9",
+    "url": "/rick-and-morty-react-app/static/js/main.16e0251c.chunk.js"
+  },
+  {
+    "revision": "3e2ef884a9383bca9de4",
+    "url": "/rick-and-morty-react-app/static/js/3.639c3c82.chunk.js"
+  },
+  {
+    "revision": "2bbec23d146c4c07a0fa",
+    "url": "/rick-and-morty-react-app/static/js/2.0326bec1.chunk.js"
+  },
+  {
+    "revision": "626f47f4063f80c7ebc9",
+    "url": "/rick-and-morty-react-app/static/css/main.e7c73cdd.chunk.css"
+  },
+  {
+    "revision": "3e2ef884a9383bca9de4",
+    "url": "/rick-and-morty-react-app/static/css/3.6dd5f68b.chunk.css"
+  },
+  {
+    "revision": "9f71d5593274de421baebacb26e9508b",
+    "url": "/rick-and-morty-react-app/index.html"
+  }
+];
