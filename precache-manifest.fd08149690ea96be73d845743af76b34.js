f6ea888f22c903f1b05bc3df2ea44170bcffd31a
kalash14 <maxim.kalashnyk@gmail.com>
2019-05-21 00:00:53 +0300

    Updates

---
+++ precache-manifest.fd08149690ea96be73d845743af76b34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bccac5526449125b2d5f",
+    "url": "/rick-and-morty-react-app/static/js/runtime~main.c5759235.js"
+  },
+  {
+    "revision": "83fb15c72575397e4d21",
+    "url": "/rick-and-morty-react-app/static/js/main.cdef9e3f.chunk.js"
+  },
+  {
+    "revision": "bb3798dc0bb5d6a9d9a6",
+    "url": "/rick-and-morty-react-app/static/js/3.eeb8abb4.chunk.js"
+  },
+  {
+    "revision": "73f1630ed9f2a201a969",
+    "url": "/rick-and-morty-react-app/static/js/2.ecf30ac2.chunk.js"
+  },
+  {
+    "revision": "83fb15c72575397e4d21",
+    "url": "/rick-and-morty-react-app/static/css/main.590ef916.chunk.css"
+  },
+  {
+    "revision": "bb3798dc0bb5d6a9d9a6",
+    "url": "/rick-and-morty-react-app/static/css/3.fe6addbe.chunk.css"
+  },
+  {
+    "revision": "3ac444284fa1684f33a08a28e2956c79",
+    "url": "/rick-and-morty-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rick-and-morty-react-app/precache-manifest.6d5ee73c05d52948be4a49684f068fe0.js"
+  "/rick-and-morty-react-app/precache-manifest.fd08149690ea96be73d845743af76b34.js"
 );
 
 workbox.clientsClaim();
