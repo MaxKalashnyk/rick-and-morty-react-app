b5e40000b6fa7ae5422eff18e353d86b0b9243cd
kalash14 <maxim.kalashnyk@gmail.com>
2019-06-09 20:46:09 +0300

    style fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import FilterPanel from "./Components/FilterPanel/FilterPanel";
 import Spinner from "./Components/Spinner/Spinner";
